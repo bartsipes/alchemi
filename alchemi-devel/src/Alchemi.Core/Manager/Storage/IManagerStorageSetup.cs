0c8d46030ef0ebc726e8304b6b5c3b517857b95d
tiborbiro <tiborbiro@9c0b43c4-e927-0410-a2d5-cf972382494e>
2005-08-30 12:13:02 +0000

    *** empty log message ***
    git-svn-id: https://alchemi.svn.sourceforge.net/svnroot/alchemi/trunk@22 9c0b43c4-e927-0410-a2d5-cf972382494e

--- alchemi-devel/src/Alchemi.Core/Manager/Storage/IManagerStorageSetup.cs
+++ alchemi-devel/src/Alchemi.Core/Manager/Storage/IManagerStorageSetup.cs
@@ -1,3 +1,25 @@
+//
+// Alchemi.Core.Manager.Storage.IManagerStorageSetup.cs
+//
+// Author:
+//   Tibor Biro (tb@tbiro.com)
+//
+// Copyright (C) 2005 Tibor Biro (tb@tbiro.com)
+//
+// This program is free software; you can redistribute it and/or modify 
+// it under the terms of the GNU General Public License as published by 
+// the Free Software Foundation; either version 2 of the License, or 
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License 
+// along with this program; if not, write to the Free Software 
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
 using System;
 
 namespace Alchemi.Core.Manager.Storage
@@ -5,7 +27,9 @@ namespace Alchemi.Core.Manager.Storage
 	/// <summary>
 	/// Sets up a specific storage structure.
 	/// 
-	/// A storage should know how to set itself up. 
+	/// A storage should know how to set itself up. In the event that the 
+	/// storage detects that it is not setup it might decide to initialize
+	/// the required data structures automatically.
 	/// </summary>
 	public interface IManagerStorageSetup
 	{
