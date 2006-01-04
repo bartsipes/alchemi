ffd38b98307bcdf52ebd36a3b19d48bdb6f03469
tiborbiro <tiborbiro@9c0b43c4-e927-0410-a2d5-cf972382494e>
2006-01-04 13:24:20 +0000

    *** empty log message ***
    git-svn-id: https://alchemi.svn.sourceforge.net/svnroot/alchemi/trunk@99 9c0b43c4-e927-0410-a2d5-cf972382494e

---
+++ alchemi-devel/src/Alchemi.Manager/Storage/ManagerStorageEnum.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Alchemi.Manager.Storage
+{
+	/// <summary>
+	/// Summary description for ManagerStorageEnum.
+	/// </summary>
+	public enum ManagerStorageEnum
+	{
+		InMemory,
+		SqlServer,
+		MySql
+	}
+}
