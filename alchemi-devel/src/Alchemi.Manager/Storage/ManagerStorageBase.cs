a791ee0324e2812e39f7587593db085cd002e01c
tiborbiro <tiborbiro@9c0b43c4-e927-0410-a2d5-cf972382494e>
2006-01-04 13:33:29 +0000

    *** empty log message ***
    git-svn-id: https://alchemi.svn.sourceforge.net/svnroot/alchemi/trunk@100 9c0b43c4-e927-0410-a2d5-cf972382494e

--- alchemi-devel/src/Alchemi.Manager/Storage/ManagerStorageBase.cs
+++ alchemi-devel/src/Alchemi.Manager/Storage/ManagerStorageBase.cs
@@ -1,3 +1,28 @@
+#region Alchemi copyright and license notice
+
+/*
+* Alchemi [.NET Grid Computing Framework]
+* http://www.alchemi.net
+* Title         :  MySqlManagerDatabaseStorage.cs
+* Project       :  Alchemi.Core.Manager.Storage
+* Created on    :  28 December 2005
+* Copyright     :  Copyright © 2005 The University of Melbourne
+*                    This technology has been developed with the support of
+*                    the Australian Research Council and the University of Melbourne
+*                    research grants as part of the Gridbus Project
+*                    within GRIDS Laboratory at the University of Melbourne, Australia.
+* Author        :  Tibor Biro (tb@tbiro.com)
+* License       :  GPL
+*                    This program is free software; you can redistribute it and/or
+*                    modify it under the terms of the GNU General Public
+*                    License as published by the Free Software Foundation;
+*                    See the GNU General Public License
+*                    (http://www.gnu.org/copyleft/gpl.html) for more 
+details.
+*
+*/
+#endregion
+
 using System;
 using System.Collections;
 
@@ -7,7 +32,7 @@ using Alchemi.Core.Manager.Storage;
 namespace Alchemi.Manager.Storage
 {
 	/// <summary>
-	/// Summary description for ManagerStorageBase.
+	/// Functionality common to all storage types. 
 	/// </summary>
 	public abstract class ManagerStorageBase
 	{
