3cc0e13eedf3db1cb49443856c4a7b38b42feaf3
tiborbiro <tiborbiro@9c0b43c4-e927-0410-a2d5-cf972382494e>
2006-01-20 21:10:44 +0000

    Removing obsolete SQL script files. Use the files under the Manager\Storage
    git-svn-id: https://alchemi.svn.sourceforge.net/svnroot/alchemi/trunk@109 9c0b43c4-e927-0410-a2d5-cf972382494e

--- alchemi-devel/sql/Alchemi_database.sql
+++
@@ -1,4 +0,0 @@
-IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = 'Alchemi')
-	DROP DATABASE [Alchemi];
-
-CREATE DATABASE [Alchemi];
--- alchemi-devel/sql/CleanAll.sql
+++
@@ -1,5 +0,0 @@
---cleans up all application and executors related stuff.
---user permissions etc. are all still there.
-DELETE FROM thread;
-DELETE FROM application;
-DELETE FROM executor;
