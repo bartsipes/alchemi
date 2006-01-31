07bf004c6a4535e1818c5ca91c337ea465dc4271
tiborbiro <tiborbiro@9c0b43c4-e927-0410-a2d5-cf972382494e>
2006-01-31 03:33:52 +0000

    Preparing for 1.0.1
    git-svn-id: https://alchemi.svn.sourceforge.net/svnroot/alchemi/trunk@118 9c0b43c4-e927-0410-a2d5-cf972382494e

--- alchemi-devel/src/Alchemi.Core/CommonAssemblyInfo.cs
+++ alchemi-devel/src/Alchemi.Core/CommonAssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
 
