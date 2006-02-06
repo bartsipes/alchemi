6fc2bcdd69523b4597d6d6661066f78cc8b9da54
tiborbiro <tiborbiro@9c0b43c4-e927-0410-a2d5-cf972382494e>
2006-02-06 21:37:59 +0000

    Updating assembly version to 1.0.2 in preparation for a new release.
    git-svn-id: https://alchemi.svn.sourceforge.net/svnroot/alchemi/trunk@129 9c0b43c4-e927-0410-a2d5-cf972382494e

--- alchemi-devel/src/Alchemi.Executor/AssemblyInfo.cs
+++ alchemi-devel/src/Alchemi.Executor/AssemblyInfo.cs
@@ -49,8 +49,8 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyInformationalVersion("1.0.2")]
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
 // Microsoft .NET Framework documentation for more information on assembly signing.
